79140a8b661d1e7de82283860cf3835d65f6a9b1
NishiOwO <nishi@nishi.boats>
2025-02-06 18:22:14 +0900

    fix

--- src/HighballVersion.pp
+++ src/HighballVersion.pp
@@ -1,12 +1,31 @@
 unit HighballVersion;
 
 interface
+type
+	THighballVersion = record
+		Major : Integer;
+		Minor : Integer;
+		Patch : Integer;
+	end;
+
+const
+	HighballVersionRaw : THighballVersion = (
+		Major : 1;
+		Minor : 0;
+		Patch : 0;
+	);
+
 function HighballGetVersion() : String;
 
 implementation
+uses
+	Sysutils;
+
 function HighballGetVersion() : String;
 begin
-	HighballGetVersion := '1.0.0';
+	HighballGetVersion :=	  IntToStr(HighballVersionRaw.Major) + '.'
+				+ IntToStr(HighballVersionRaw.Minor) + '.'
+				+ IntToStr(HighballVersionRaw.Patch) + '.';
 end;
 
 end.
