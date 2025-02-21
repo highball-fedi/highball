8c7bda058c053f72c55e93de146c519437fbc5ff
NishiOwO <nishi@nishi.boats>
2025-02-22 04:15:02 +0900

    add THighballDatabase

--- src/HighballDatabase.pp
+++ src/HighballDatabase.pp
@@ -1,14 +1,22 @@
 unit HighballDatabase;
 
 interface
+uses
+	{$if defined(POSTGRES)}
+	Postgres,
+	PQConnection;
+	{$elseif defined(SQLITE)}
+	SQLite3,
+	SQLite3Conn;
+	{$endif}
+
+type
+	{$if defined(POSTGRES)}
+	THighballDatabase = TPQConnection;
+	{$elseif defined(SQLITE)}
+	THighballDatabase = TSQLite3Connection;
+	{$endif}
 
 implementation
-uses
-	{$ifdef POSTGRES}
-	Postgres;
-	{$endif}
-	{$ifdef SQLITE}
-	SQLite3;
-	{$endif}
 
 end.
